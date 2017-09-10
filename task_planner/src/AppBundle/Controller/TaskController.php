89f8cf54ad02123c20634a5103c6fdce4009f287
Joanna Trojak <joannatrojak19@gmail.com>
2017-09-10 10:45:33 +0200

    initial commit and add new task form

---
+++ task_planner/src/AppBundle/Controller/TaskController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Task; 
+
+class TaskController extends Controller
+{
+    /**
+     * 
+     * @Route("/add")
+     */
+    public function addTastAction(Request $request) {
+        
+        $task = new Task(); 
+        
+        $form = $this->createFormBuilder($task)
+                ->add('name')
+                ->add('description')
+                ->add('dueDate')
+                ->getForm(); 
+        $form->handleRequest($request); 
+        
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $task = $form->getData(); 
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+            return new Response('<body>Task added</body>');
+        }
+        return $this->render('new_task.html.twig', array(
+            'form' => $form->createView(), 
+             ));
+        
+    }
+}
